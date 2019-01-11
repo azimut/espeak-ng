bec2d80c2010d12ce2a843229faf4634aba2be68
azimut <azimut.github@protonmail.com>
2019-01-11 20:55:09 -0300

    t-trash

--- t/test.lisp
+++ t/test.lisp
@@ -1,10 +1,42 @@
 (in-package #:libespeak-ng)
 
+(cffi:defcallback ctestr :int ((wav :pointer) (num :int) (pev :pointer))
+  (declare (ignore pev))
+  (if (null num)
+      (progn
+        (format T "~a" num)
+        0)
+      (progn
+        (format T "~a" num)
+        1)))
+
+(defun test (&optional (text "crazy"))
+  (let ((len-text (1+ (length text))))
+    (with-espeak (:AUDIO_OUTPUT_SYNCHRONOUS 2048 (cffi:null-pointer) 0)
+      (cffi:with-foreign-strings ((lang "en")
+                                  (ctext text))
+        (cffi:with-foreign-object (text-size 'size)
+          (espeak_setsynthcallback (cffi:callback ctestr))
+          (espeak_setvoicebyname lang)
+          (setf (cffi:mem-ref text-size 'size)
+                len-text)
+          (print
+           (espeak_synth ctext text-size 0
+                         :POS_CHARACTER
+                         0
+                         ESPEAKCHARS_AUTO
+                         (cffi:null-pointer)
+                         (cffi:null-pointer)))
+          (espeak_synchronize)
+          ;;(sleep 3)
+          )))))
+
+
 (defun test ()
   (with-espeak (:audio_output_retrieval 10000 (cffi:null-pointer) 0)
     (cffi:with-foreign-strings ((lang "en")
                                 (word "crazy"))
-      (espeak_setsynthcallback (cffi:callback ctestr))      
+      (espeak_setsynthcallback (cffi:callback ctestr))
       (espeak_setvoicebyname lang)
       (cffi:with-foreign-object (l 'size)
         (setf (cffi:mem-ref lang 'size) 6)
